3dc880062d6d9ba53c72029c7d0a74305833df4e
DevXian <coolman555@me.com>
2021-11-06 14:45:53 +0900

    feat: file and error report

--- api/index.js
+++ api/index.js
@@ -1,6 +1,14 @@
 const express = require("express");
 const router = express.Router();
 const si = require('systeminformation');
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
+
+/* 에러보고 */
+const errorReport = (res, err) => {
+    console.error(`Throw Exception -> ${err}`);
+    res.status(500).send(err)
+}
 
 /* 시스템 정보 */
 router.get("/info", async (req, res) => {
@@ -9,21 +17,21 @@ router.get("/info", async (req, res) => {
     await si.osInfo().then(({platform, distro, release}) => {
         obj = Object.assign(obj, {platform, distro, release})
     }).catch((err) => {
-        console.error(`Throw Exception -> ${err}`);
+        errorReport(res, err)
     })
 
     // CPU 정보
     await si.cpu().then(({brand}) => {
         obj = Object.assign(obj, {brand})
     }).catch((err) => {
-        console.error(`Throw Exception -> ${err}`);
+        errorReport(res, err)
     })
 
     // Ram 정보
     await si.mem().then(({total}) => {
         obj = Object.assign(obj, {total})
     }).catch((err) => {
-        console.error(`Throw Exception -> ${err}`);
+        errorReport(res, err)
     })
 
     res.json(obj);
@@ -40,7 +48,7 @@ router.get("/usage", async (req, res) => {
         }, 0) / cpus.length
         obj = Object.assign(obj, {cpu: usage.toFixed(2)})
     }).catch((err) => {
-        console.error(`Throw Exception -> ${err}`);
+        errorReport(res, err)
     })
 
     // 메모리 사용률
@@ -48,7 +56,7 @@ router.get("/usage", async (req, res) => {
         const usage = used/total*100
         obj = Object.assign(obj, {ram:usage.toFixed(2)})
     }).catch((err) => {
-        console.error(`Throw Exception -> ${err}`);
+        errorReport(res, err)
     })
 
     // 네트워크 사용률
@@ -56,7 +64,7 @@ router.get("/usage", async (req, res) => {
         const {rx_bytes, tx_bytes} = props[0]
         obj = Object.assign(obj, {rx_bytes, tx_bytes})
     }).catch((err) => {
-        console.error(`Throw Exception -> ${err}`);
+        errorReport(res, err)
     })
 
     res.json(obj);
@@ -68,16 +76,27 @@ router.get("/process", async (req, res) => {
     await si.processes().then((result) => {
         obj = Object.assign(obj, result)
     }).catch((err) =>{
-        console.error(`Throw Exception -> ${err}`);
+        errorReport(res, err)
     })
 
     res.json(obj);
 })
 
-/* 파일 탐색 
-router.get("/file" (req, res) => {
+/* 파일 탐색  */
+router.get("/file", async (req, res) => {
+    try {
+        const {path} = req?.query
 
+        const { stdout, stderr } = await exec(`ls ${path}`);
+
+        res.send(stdout)
+        
+        if(stderr) {
+            errorReport(res, stderr)
+        }
+    } catch (err){
+        errorReport(res, err)
+    };
 })
-*/
 
 module.exports = router;
